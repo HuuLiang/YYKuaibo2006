e3cfc8ee25d460ce6e90219ee0a844c5aaca3c76
Yu Xulu <tonyfish@qq.com>
2016-03-24 17:51:08 +0800

    Use local wechat payment cnfiguration if no remote one.

--- YYKuaibo/Class/Base/Model/YYKPaymentConfig.m
+++ YYKuaibo/Class/Base/Model/YYKPaymentConfig.m
@@ -94,7 +94,7 @@ static YYKPaymentConfig *_shardConfig;
 
 - (void)setAsCurrentConfig {
     YYKPaymentConfig *currentConfig = [[self class] sharedConfig];
-    currentConfig.weixinInfo = self.weixinInfo;
+    currentConfig.weixinInfo = self.weixinInfo ?: [YYKWeChatPaymentConfig defaultConfig];
     currentConfig.iappPayInfo = self.iappPayInfo;
     currentConfig.alipayInfo = self.alipayInfo;
 }
