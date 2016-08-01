733a8c8ab18c230450f9abdaec49d75796f312ad
Yu Xulu <tonyfish@qq.com>
2016-08-01 15:29:37 +0800

    Revert "change pageSize"
    This reverts commit 57829d92d60372b3be6f6ec7ad9130236f173844.

--- YYKuaibo/Class/Video/Model/YYKChannelProgramModel.m
+++ YYKuaibo/Class/Video/Model/YYKChannelProgramModel.m
@@ -21,9 +21,7 @@
     }
     
     @weakify(self);
-    NSDictionary *params = @{@"columnId":columnId,
-                             @"page":@(page),
-                             @"pageSize":@(10)};
+    NSDictionary *params = @{@"columnId":columnId, @"page":@(page)};
     BOOL ret = [self requestURLPath:YYK_CHANNEL_PROGRAM_URL withParams:params responseHandler:^(YYKURLResponseStatus respStatus, NSString *errorMessage) {
         @strongify(self);
         if (!self) {
