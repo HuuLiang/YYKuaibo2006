684ee583e5731b541d13ee5c0dc1074804323e1d
Yu Xulu <tonyfish@qq.com>
2016-08-31 09:59:09 +0800

    BatchBuild scripts for H5 channel

---
+++ BatchBuild_H5Channel/gitclone.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+GIT_PROJECT_NAME="YYKuaibo"
+
+RESPO_URL="git@git.coding.net:seanyue/$GIT_PROJECT_NAME.git"
+
+STORAGE_DIR="$GIT_PROJECT_NAME"
+
+GIT_EXEC="/usr/bin/git"
+
+CURRENT_USER_HOME=`cd ~;pwd`
+LOCAL_PROJECT_DIR="$CURRENT_USER_HOME/$STORAGE_DIR"
+THREAD_NUM=4
+cd ~
+
+if [ -z "$GIT_PROJECT_NAME" ]; then 
+	echo "git project directory is empty"
+	exit 1
+fi
+if [ -d "$LOCAL_PROJECT_DIR" ]; then 
+	rm -rf $LOCAL_PROJECT_DIR >/dev/null 2>&1
+	if [ $? != 0 ]; then
+       		echo "deleting $LOCAL_PROJECT_DIR directory failed"
+        	exit 1
+	fi
+fi
+
+eval "$GIT_EXEC clone $RESPO_URL $LOCAL_PROJECT_DIR > /dev/null 2>&1"
+
+if [ $? != 0 ]; then
+	echo "$LOCAL_PROJECT_DIR can not be download"
+        exit 1
+fi
+
+for i in `eval echo {1..$THREAD_NUM}`; do
+	if [ -d "$LOCAL_PROJECT_DIR$i" ]; then
+		rm -rf $LOCAL_PROJECT_DIR$i >/dev/null 2>&1
+		if [ $? != 0 ]; then
+                	echo "deleting $LOCAL_PROJECT_DIR$i backup directory failed"
+                	exit 1
+        	fi
+	fi
+	cp -rf $LOCAL_PROJECT_DIR $LOCAL_PROJECT_DIR$i >/dev/null 2>&1
+done
---
+++ BatchBuild_H5Channel/pack.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+PRG="$0"
+
+PROJECT_NAME="YYKuaibo"
+SCHEME="$PROJECT_NAME"
+
+CURRENT_USER_HOME=`cd ~;pwd`
+PROJECT_DIR="$CURRENT_USER_HOME/$PROJECT_NAME"
+IPA_DES_DIR="$CURRENT_USER_HOME/${PROJECT_NAME}IpaDir"
+CHANNELNO_PRFIX="IOS_H5_"
+CHANNELNO=
+PRGDIR=`dirname $PRG`
+PRGABSOLUTEDIR=`cd $PRGDIR;pwd`
+IPA_EXEC="$PRGABSOLUTEDIR/ipa.sh"
+
+MINPACKAGENO=$1
+MAXPACKAGENO=$2
+
+if [ ! -d "$IPA_DES_DIR" ]; then
+	mkdir -p $IPA_DES_DIR 
+fi
+
+THREAD_NUM=4
+
+mkfifo packpipe
+exec 9<>packpipe
+
+rm packpipe
+
+for i in `eval echo {1..$THREAD_NUM}`; do
+	echo "$i" 1>&9
+done
+
+cd ~
+
+while [ $MINPACKAGENO -le $MAXPACKAGENO ]; do
+	read -u 9 seq
+	{
+		MINPACKAGENOPADDING="`printf "%3d" $MINPACKAGENO | tr " " 0`"
+		if [ -d "$IPA_DES_DIR/$MINPACKAGENOPADDING" ]; then 
+			rm -rf $IPA_DES_DIR/$MINPACKAGENOPADDING > /dev/null 2>&1 
+		fi
+		mkdir -p $IPA_DES_DIR/$MINPACKAGENOPADDING
+		CHANNELNO="$CHANNELNO_PRFIX`printf "%8d" $MINPACKAGENO | tr " " 0`"
+		eval "$IPA_EXEC $PROJECT_DIR$seq $CHANNELNO $MINPACKAGENO $IPA_DES_DIR/$MINPACKAGENOPADDING $SCHEME"
+		echo "$seq" 1>&9
+	}&
+	let MINPACKAGENO=MINPACKAGENO+1
+done
+
+wait
+
+echo "successfully package"
+
+exec 9>&-
